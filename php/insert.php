727a96f2cf7143a22f89e7ef091f8087cacf7c64
Maiko Matsuoka <117214172+NIMUchanco@users.noreply.github.com>
2023-09-11 11:14:06 -0400

    Added delete.php and style.css files.

--- php/insert.php
+++ php/insert.php
@@ -4,6 +4,72 @@
     error_reporting(E_ALL);
 
     require_once "_includes/db_connect.php";
+    $results = [];
+
+
+    function selectId($link) {
+        //get selected id
+        $id = $_POST["id"];
+        $results = [];
+
+        //prepare statement passing in the link to the database and the query
+        $query = "SELECT * FROM account_book WHERE id = ?";
+
+        if ($stmt = mysqli_prepare($link, $query)) {
+            //bind parameters
+            mysqli_stmt_bind_param($stmt, "i", $id);
+            
+            //execute the statement
+            mysqli_stmt_execute($stmt);
+            
+            //get the result
+            $result = mysqli_stmt_get_result($stmt);
+
+            //fetch the result as an associative array
+            $row = mysqli_fetch_assoc($result);
+
+            return $row;
+            
+        } else {
+            throw new Exception("No ID found");
+        }
+    }
+
+    function updateData($link) {
+        $results = [];
+
+        //query to replace the record
+        $query = "UPDATE account_book SET date = ?, item = ?, price = ?, type = ? WHERE id = ?";
+
+        //prepare statement passing in the link to the database and the query
+        $stmt = mysqli_prepare($link, $query);
+
+        //get selected id and other values
+        $id= $_POST["id"];
+        $date = $_POST["date"];
+        $item = $_POST["item"];
+        $price = $_POST["price"];
+        $type = $_POST["type"];
+
+        if ($stmt) {
+            //bind parameters
+            mysqli_stmt_bind_param($stmt, "ssdsi", $date, $item, $price, $type, $id);
+            //execute the statement
+            mysqli_stmt_execute($stmt);
+
+            //if no updated rows, throw an exception
+            if (mysqli_stmt_affected_rows($stmt) === 0) {
+                throw new Exception("No rows updated");
+            }
+
+            //add updated rows to the results array
+            $results[] = [
+                "updateData() affected rows" => mysqli_stmt_affected_rows($stmt)
+            ];
+
+            return mysqli_stmt_affected_rows($stmt);
+        }
+    }
 
     function insertData($link) {
         $results = [];
@@ -54,7 +120,12 @@
     }
 
     try {
-        $results[] = insertData($link);
+        
+        if(selectId($link)) {
+            $results = updateData($link);
+        } else {
+            $results = insertData($link);
+        }
 
     } catch (Exception $error) {
         echo "error occurs: " . $error->getMessage();
